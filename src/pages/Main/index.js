5eb22f79021aedfd84a688f28d8a79d9d2d1b1aa
Charles Loureiro de Deus <charliedeus@gmail.com>
2019-11-04 21:15:47 -0300

    Loading api data

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -74,7 +74,7 @@ export default class Main extends Component {
             onChange={this.handleInputChange}
           />
 
-          <SubmitButton loading={loading}>
+          <SubmitButton loading={loading ? 1 : 0}>
             {loading ? (
               <FaSpinner color="#FFF" size="14" />
             ) : (
